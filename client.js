b316325c3507c76f7d3a63b98c32711e4b321d8d
James-Sangalli <j.l.sangalli@gmail.com>
2023-10-22 16:44:30 +1100

    final

--- client.js
+++ client.js
@@ -1,65 +1,102 @@
-'use strict'
+"use strict";
 
-const { PeerRPCClient }  = require('grenache-nodejs-http')
-const Link = require('grenache-nodejs-link')
+const { PeerRPCClient } = require("grenache-nodejs-http");
+const Link = require("grenache-nodejs-link");
 
 const link = new Link({
-  grape: 'http://127.0.0.1:30001',
-  requestTimeout: 10000
-})
-link.start()
+  grape: "http://127.0.0.1:30001",
+  requestTimeout: 10000,
+});
+link.start();
 
 // discover RPC nodes on the network that process orders
-link.lookup('order_book_worker', {}, (err, data) => {
-  if(err) console.log(err)
-})
+link.lookup("order_book_worker", {}, (err, data) => {
+  if (err) console.log(err);
+});
 
-const peer = new PeerRPCClient(link, {})
-peer.init()
+const peer = new PeerRPCClient(link, {});
+peer.init();
+
+// generate a random number and use it as a userId so that orders can be traced back
+const userId = Math.floor(Math.random() * 9999999999);
 
 const clientOrderBook = {
   buy: [],
   sell: [],
-}
+};
 
 function buy() {
   const sampleBuyOrder = {
-    type: 'buy',
-    coin: 'BTC',
+    userId,
+    type: "buy",
+    coin: "BTC",
     amount: Math.floor(Math.random() * 100) + 1,
     usdPrice: Math.floor(Math.random() * 5) + 1,
-  }
+    orderId: Math.floor(Math.random() * 999999999) + 1,
+  };
+  // Share our buy order with the network
+  link.put(sampleBuyOrder, () => {});
   // add a buy order
-  peer.request('order_book_worker', sampleBuyOrder, { timeout: 100000 }, (err, msg) => {
-    if (err) throw err
-    console.log(msg)
-    clientOrderBook.buy.push(sampleBuyOrder)
-  })
+  peer.request(
+    "order_book_worker",
+    sampleBuyOrder,
+    { timeout: 100000 },
+    (err, msg) => {
+      if (err) throw err;
+      console.log(msg);
+      clientOrderBook.buy.push(sampleBuyOrder);
+    }
+  );
+  // getStatusOfOrderById(sampleBuyOrder.orderId)
 }
 
 function sell() {
   const sampleSellOrder = {
-    type: 'sell',
-    coin: 'BTC',
+    userId,
+    type: "sell",
+    coin: "BTC",
     amount: Math.floor(Math.random() * 100) + 1,
     usdPrice: Math.floor(Math.random() * 5) + 1,
-  }
+    orderId: Math.floor(Math.random() * 999999999) + 1,
+  };
+  // Share our sell order with the network
+  link.put(sampleSellOrder, () => {});
   // add a sell order
-  peer.request('order_book_worker', sampleSellOrder, { timeout: 100000 }, (err, msg) => {
-    if (err) throw err
-    console.log(msg)
-    clientOrderBook.sell.push(sampleSellOrder)
-  })
+  peer.request(
+    "order_book_worker",
+    sampleSellOrder,
+    { timeout: 100000 },
+    (err, msg) => {
+      if (err) throw err;
+      console.log(msg);
+      clientOrderBook.sell.push(sampleSellOrder);
+    }
+  );
+  // getStatusOfOrderById(sampleSellOrder.orderId)
 }
+
+// function getStatusOfOrderById(orderId) {
+//   setTimeout(() => {
+//     peer.request(
+//         "order_book_worker",
+//         orderId,
+//         { timeout: 100000 },
+//         (err, msg) => {
+//           if (err) throw err;
+//           console.log(msg);
+//         }
+//     );
+//     getStatusOfOrderById(orderId);
+//   }, 10000);
+// }
 
 function init() {
-  buy()
-  sell()
+  // TODO: allow a client to set an expiry date or cancel their order
+  buy();
+  sell();
   setTimeout(() => {
-    init()
-  }, 10000)
+    init();
+  }, 10000);
 }
 
-init()
-
-
+init();
--- server.js
+++ server.js
@@ -1,126 +1,156 @@
-'use strict'
+"use strict";
 
-const { PeerRPCServer }  = require('grenache-nodejs-http')
-const Link = require('grenache-nodejs-link')
+const { PeerRPCServer } = require("grenache-nodejs-http");
+const Link = require("grenache-nodejs-link");
 const async = require("async");
 
 // The order book containing all orders on the network
 const orderBook = {
-    buy: [],
-    sell: [],
-}
+  buy: [],
+  sell: [],
+};
+
+// OrderId - boolean
+const filledOrders = {}
 
 const link = new Link({
-    grape: 'http://127.0.0.1:30001'
-})
-link.start()
+  grape: "http://127.0.0.1:30001",
+});
+link.start();
 
-const peer = new PeerRPCServer(link, {})
-peer.init()
+const peer = new PeerRPCServer(link, {});
+peer.init();
 
-const service = peer.transport('server')
-service.listen(1337)
+const port = 1024 + Math.floor(Math.random() * 1000)
+const service = peer.transport("server");
+service.listen(port); //(1337);
 
 setInterval(() => {
-    link.announce('order_book_worker', service.port, {})
-}, 1000)
+  link.announce("order_book_worker", service.port, {});
+}, 1000);
 
-service.on('request', (rid, key, order, handler) => {
-    // TODO handle the request type to check for updates to a client's order book
-    handleOrderSubmission(order, handler)
-    handler.reply(null, `${order.type} order received`)
-})
+service.on("request", (rid, key, payload, handler) => {
+  // TODO handle requests to check for updates to a client's order book by userId
+  // if(typeof payload === 'number') {
+  //   addOrderFulfilledCheckToQueueAndProcess.push(payload, (err, res) => {
+  //     if (err) {
+  //       console.error(`Error processing enquiry: ${err}`);
+  //     } else {
+  //       handler.reply(null, res);
+  //     }
+  //   })
+  // } else {
+    handleOrderSubmission(payload, handler);
+    handler.reply(null, `${payload.type} order received`);
+  // }
+});
 
 /*
-* @dev process orders from clients
-* @dev broadcasts the order to other nodes on the network
-* @dev adds the order to the queue for processing
-* @dev checks if the order can be matched and modifies the order book
-* @param order - the order submitted by the client
-* */
+ * @dev process orders from clients
+ * @dev broadcasts the order to other nodes on the network
+ * @dev adds the order to the queue for processing
+ * @dev checks if the order can be matched and modifies the order book
+ * @param order - the order submitted by the client
+ * */
 function handleOrderSubmission(order) {
-    // broadcast the order to other nodes on the network
-    link.put(order, () => {
-        // process order via the queue
-        addOrderToQueueAndProcess.push(order, (err, res) => {
-            if (err) {
-                console.error(`Error processing order: ${err}`);
-            } else {
-                console.log(`processed: ${res}`)
-            }
-        });
-    })
+  // process order via the queue
+  addOrderToQueueAndProcess.push(order, (err, res) => {
+    if (err) {
+      console.error(`Error processing order: ${err}`);
+    } else {
+      console.log(`processed: ${res}`);
+    }
+  });
 }
 
+function getIsOrderFulfilled(orderId) {
+  return !!filledOrders[orderId];
+}
+
+const addOrderFulfilledCheckToQueueAndProcess = async.queue((orderId, callback) => {
+  setTimeout(() => {
+    callback(null, getIsOrderFulfilled(orderId))
+  }, 1000)
+}, 1)
+
 /*
-* @dev add an order to the queue and check for matches
-* @param order - the order to process
-* */
+ * @dev add an order to the queue and check for matches
+ * @param order - the order to process
+ * */
 const addOrderToQueueAndProcess = async.queue((order, callback) => {
-    setTimeout(() => {
-        processOrder(order)
-        console.log(`Order match check done for: ${JSON.stringify(order)}`);
-        callback(null, true);
-    }, 1000); // delay simulation
+  setTimeout(() => {
+    processOrder(order);
+    console.log(`Order match check done for: ${JSON.stringify(order)}`);
+    callback(null, true);
+  }, 1000); // delay simulation
 }, 1);
 
 function processOrder(order) {
-    // TODO support multiple currency pairs
-    if (order.type === 'buy') {
-       processBuyOrder(order)
-    } else {
-       processSellOrder(order)
-    }
+  if (order.type === "buy") {
+    processBuyOrder(order);
+  } else {
+    processSellOrder(order);
+  }
 }
 
 /*
-* @dev process a buy order by checking for matches
-* @param buyOrder - the buy order to match
-* */
+ * @dev process a buy order by checking for matches
+ * @param buyOrder - the buy order to match
+ * */
 function processBuyOrder(buyOrder) {
-    orderBook.buy.push(buyOrder)
-    for(let i = 0; i < orderBook.sell.length; i++) {
-        if(buyOrder.usdPrice === orderBook.sell[i].usdPrice && buyOrder.coin === orderBook.sell[i].coin) {
-            console.log('order match found')
-            if(buyOrder.quantity === orderBook.sell[i].quantity) {
-                // perfect match, buy order is never added and the sell order is removed
-                orderBook.sell.splice(i, 1);
-            } else if(buyOrder.quantity > orderBook.sell[i].quantity) {
-                // the buy order is greater than the sell order, remove the sell order, decrease the buy order and add the modified buy order
-                buyOrder.quantity -= orderBook.sell[i].quantity
-                orderBook.sell.splice(i, 1);
-                orderBook.buy.push(buyOrder)
-            } else {
-                // the sell order is greater than the buy order, don't add the buy order and decrease the sell order
-                orderBook.sell[i].quantity -= buyOrder.quantity
-            }
-        }
+  orderBook.buy.push(buyOrder);
+  for (let i = 0; i < orderBook.sell.length; i++) {
+    if (
+      buyOrder.usdPrice === orderBook.sell[i].usdPrice &&
+      buyOrder.coin === orderBook.sell[i].coin
+    ) {
+      console.log("order match found");
+      if (buyOrder.amount === orderBook.sell[i].amount) {
+        console.log("exact match, remove both orders");
+        filledOrders[buyOrder.orderId] = true;
+        filledOrders[orderBook.sell[i].orderId] = true;
+        orderBook.sell.splice(i, 1);
+      } else if (buyOrder.amount > orderBook.sell[i].amount) {
+        console.log("buy > sell, decrease buy amount and remove sell order");
+        orderBook.buy[orderBook.buy.length - 1].amount -= orderBook.sell[i].amount;
+        filledOrders[orderBook.sell[i].orderId] = true;
+        orderBook.sell.splice(i, 1);
+      } else {
+        console.log("sell > buy, decrease sell amount and remove buy order");
+        orderBook.sell[i].amount -= buyOrder.amount;
+        filledOrders[buyOrder.orderId] = true;
+        orderBook.buy.pop();
+      }
     }
+  }
 }
 
 /*
-* @dev process a sell order by checking for matches
-* @param sellOrder - the sell order to match
-* */
+ * @dev process a sell order by checking for matches
+ * @param sellOrder - the sell order to match
+ * */
 function processSellOrder(sellOrder) {
-    orderBook.sell.push(sellOrder)
-    for(let i = 0; i < orderBook.buy.length; i++) {
-        if(sellOrder.usdPrice === orderBook.buy[i].usdPrice && sellOrder.coin === orderBook.buy[i].coin) {
-            console.log('order match found')
-            if(sellOrder.quantity === orderBook.buy[i].quantity) {
-                // perfect match, remove both the buy and sell order
-                orderBook.buy.splice(i, 1);
-                orderBook.sell.pop()
-            } else if(sellOrder.quantity > orderBook.buy[i].quantity) {
-                // the sell order is greater than the buy order, remove the buy order, decrease the sell order and add the modified sell order
-                sellOrder.quantity -= orderBook.buy[i].quantity
-                orderBook.buy.splice(i, 1);
-                orderBook.sell.push(sellOrder)
-            } else {
-                // the sell order is greater than the buy order, don't add the sell order and decrease the buy order
-                orderBook.buy[i].quantity -= sellOrder.quantity
-            }
-        }
+  orderBook.sell.push(sellOrder);
+  for (let i = 0; i < orderBook.buy.length; i++) {
+    if (
+      sellOrder.usdPrice === orderBook.buy[i].usdPrice &&
+      sellOrder.coin === orderBook.buy[i].coin
+    ) {
+      console.log("order match found");
+      if (sellOrder.amount === orderBook.buy[i].amount) {
+        console.log("exact match, remove both orders");
+        orderBook.buy.splice(i, 1);
+        orderBook.sell.pop();
+      } else if (sellOrder.amount > orderBook.buy[i].amount) {
+        console.log("sell > buy, decrease sell amount and remove buy order");
+        orderBook.sell[orderBook.sell.length - 1].amount -=
+          orderBook.buy[i].amount;
+        orderBook.buy.splice(i, 1);
+      } else {
+        console.log("buy > sell, decrease buy amount and remove sell order");
+        orderBook.buy[i].amount -= sellOrder.amount;
+        orderBook.sell.pop();
+      }
     }
+  }
 }
-
